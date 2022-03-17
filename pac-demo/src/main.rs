68e7c21ccd2d0cc46f5315815c5b77fa7a1354d6
Fedor Chervyakov <fchervyakov@gmail.com>
2022-03-18 01:43:04 +0300

    pac-demo: src: main: add TODOs and use interrupt-free closure

--- pac-demo/src/main.rs
+++ pac-demo/src/main.rs
@@ -7,24 +7,10 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_itm as _; // logs messages over ITM; requires ITM support
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
-use cortex_m::peripheral::{
-    syst,
-    SYST,
-};
 use cortex_m_rt::entry;
 use stm32f4::stm32f429;
 use rtt_target::{rtt_init_print, rprintln};
 
-
-fn delay(systick: &mut SYST, delay_ticks: u32) -> () {
-    systick.set_reload(delay_ticks);
-    systick.clear_current();
-    systick.enable_counter();
-    
-    while !systick.has_wrapped() {
-        // Loop
-    }
-}
 
 #[entry]
 fn main() -> ! {
@@ -33,26 +19,34 @@ fn main() -> ! {
     let cp = cortex_m::Peripherals::take().unwrap();
     let p = stm32f429::Peripherals::take().unwrap();
 
-    let mut systick = cp.SYST;
-    systick.set_clock_source(syst::SystClkSource::Core);
+    // Critical section, interrupt-free closure
+    cortex_m::interrupt::free(|_cs| {
+        // TODO: Setup clocks
+        // Goes sorta like this ???
+        // We start on the HSI
+        // 1. Configure PLL
+        // 1.1 Select HSE osc as pll source
+        // 1.2 Setup division factors
+        // 2. Use PLL as the SYSCLK
 
-    p.RCC.ahb1enr.write(|w| w.gpiogen().set_bit());
+        // Enable peripherals
+        p.RCC.ahb1enr.write(|w| w.gpiogen().set_bit());
+        // TODO: enable timer
 
-    let gpiog = p.GPIOG;
+        // Configure pin PG13 as push-pull output with pull down
+        p.GPIOG.moder.write(|w| w.moder13().output()); // set general purpose output mode
+        p.GPIOG.otyper.write(|w| w.ot13().clear_bit()); // output push-pull
+        p.GPIOG.pupdr.write(|w| w.pupdr13().pull_down()); // enable pull-down
 
-    gpiog.moder.write(|w| w.moder13().output()); // set general purpose output mode
-    gpiog.otyper.write(|w| w.ot13().clear_bit());  // output push-pull
-    gpiog.pupdr.write(|w| unsafe { w.pupdr13().bits(2) }); // enable pull-down
+        // TODO: configure timer
+    });
 
     loop {
-        gpiog.bsrr.write(|w| w.bs13().set_bit());
+        // TODO: trigger on timer ??? use interrupts???
+        p.GPIOG.bsrr.write(|w| w.bs13().set_bit());
         rprintln!("Led ON");
 
-        delay(&mut systick, 100_000_000);
-
-        gpiog.bsrr.write(|w| w.br13().set_bit());
+        p.GPIOG.bsrr.write(|w| w.br13().set_bit());
         rprintln!("Led OFF");
-
-        delay(&mut systick, 100_000_000);
     }
 }
