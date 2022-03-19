6cce42605a554b2d08badbfbebbe9ecd8593f317
Fedor Chervyakov <fchervyakov@gmail.com>
2022-03-19 22:51:11 +0300

    pac-demo: src: main: configure HSE as SYSCLK

--- pac-demo/src/main.rs
+++ pac-demo/src/main.rs
@@ -8,16 +8,17 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m_rt::entry;
-use stm32f4::stm32f429;
+use stm32f4::stm32f429 as f4;
 use rtt_target::{rtt_init_print, rprintln};
 
+const XTAL_FREQUENCY: u32 = 8_000_000; // Hz
 
 #[entry]
 fn main() -> ! {
     rtt_init_print!();
 
     let cp = cortex_m::Peripherals::take().unwrap();
-    let p = stm32f429::Peripherals::take().unwrap();
+    let p = f4::Peripherals::take().unwrap();
 
     // Critical section, interrupt-free closure
     cortex_m::interrupt::free(|_cs| {
@@ -28,6 +29,11 @@ fn main() -> ! {
         // 1.1 Select HSE osc as pll source
         // 1.2 Setup division factors
         // 2. Use PLL as the SYSCLK
+        //
+
+        // SYSCLK from HSE
+        p.RCC.cr.write(|w| w.hseon().on());
+        p.RCC.cfgr.write(|w| w.sw().hse());
 
         // Enable peripherals
         p.RCC.ahb1enr.write(|w| w.gpiogen().set_bit());
@@ -41,12 +47,28 @@ fn main() -> ! {
         // TODO: configure timer
     });
 
+    match p.RCC.cfgr.read().sws().variant() {
+        Some(f4::rcc::cfgr::SWS_A::HSI) => rprintln!("HSI"),
+        Some(f4::rcc::cfgr::SWS_A::HSE) => rprintln!("HSE"),
+        Some(f4::rcc::cfgr::SWS_A::PLL) => rprintln!("PLL"),
+        None => rprintln!("Else!")
+    };
+
+    while p.RCC.cr.read().hserdy().is_not_ready() {}
+
+    match p.RCC.cfgr.read().sws().variant() {
+        Some(f4::rcc::cfgr::SWS_A::HSI) => rprintln!("HSI"),
+        Some(f4::rcc::cfgr::SWS_A::HSE) => rprintln!("HSE"),
+        Some(f4::rcc::cfgr::SWS_A::PLL) => rprintln!("PLL"),
+        None => rprintln!("Else!")
+    };
+
     loop {
         // TODO: trigger on timer ??? use interrupts???
-        p.GPIOG.bsrr.write(|w| w.bs13().set_bit());
-        rprintln!("Led ON");
+        //p.GPIOG.bsrr.write(|w| w.bs13().set_bit());
+        //rprintln!("Led ON");
 
-        p.GPIOG.bsrr.write(|w| w.br13().set_bit());
-        rprintln!("Led OFF");
+        //p.GPIOG.bsrr.write(|w| w.br13().set_bit());
+        //rprintln!("Led OFF");
     }
 }
